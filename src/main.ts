292201e942ec2c26a4953ef78f421d90ca955fe9
leixq <cathead870@163.com>
2025-06-20 21:45:30 +0800

    chore 🔥:初始化项目

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "@/styles/index.scss";
+import App from "./page/index.vue";
+
+createApp(App).mount("#app");
---
+++ src/page/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container"></main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url("@/assets/images/背景.png") no-repeat center center;
+  background-size: cover;
+}
+</style>

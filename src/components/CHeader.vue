95359c6bb366ebfddf984fd746bbd312a2527386
leixq <cathead870@163.com>
2025-06-22 12:21:31 +0800

    style 🎨:引入字体

--- src/components/CHeader.vue
+++ src/components/CHeader.vue
@@ -1,6 +1,6 @@
 <!-- 顶部标题 -->
 <template>
-  <header class="header"></header>
+  <header class="header">山东省旅游指标监控平台</header>
 </template>
 
 <script setup lang="ts"></script>
@@ -12,7 +12,15 @@
   top: 12px;
   width: calc(100% - 24px);
   height: 87px;
-  background: url("@/assets/images/顶部标题.png") no-repeat center center;
+  background: url('@/assets/images/顶部标题.png') no-repeat center center;
   background-size: 100% 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  font-weight: 800;
+  letter-spacing: 12px;
+  color: rgba(230, 239, 253);
+  font-family: 'MStiffHeiPRC';
 }
 </style>
